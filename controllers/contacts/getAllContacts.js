191f370e2fb634a365c22b7e23076e9b05a3f5f3
Andrey14121992 <andrey1477@ukr.net>
2023-03-13 22:29:40 +0200

    5

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,18 +1,18 @@
 const { Contact } = require("../../models");
 
 const getAllContacts = async (req, res) => {
-  const { _id } = req.user;
+  const { _id: owner } = req.user;
   const { page = 1, limit = 20, favorite = false } = req.query;
 
   const skip = (page - 1) * limit;
   const contacts = await Contact.find(
-    { owner: _id, favorite },
+    { owner, favorite },
     "-createdAt -updatedAt",
     {
       skip,
       limit: +limit,
     }
-  ).populate("owner", "_id email");
+  ).populate("owner", "email");
   res.json({ status: "succsess", code: 200, data: contacts });
 };
 
